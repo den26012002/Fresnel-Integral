dc25b1396ff3b6eace57202f5c37b75465c95a58
den26012002 <den26012002@mail.ru>
2021-12-29 18:33:49 +0300

    fix: change intensivitiest to amplitudes

--- Интеграл Френеля/FresnelIntegral.cpp
+++ Интеграл Френеля/FresnelIntegral.cpp
@@ -1,6 +1,6 @@
 #include "FresnelIntegral.h"
 
-FresnelIntegral::FresnelIntegral(double _lambda, double _z, const std::vector<std::vector<double>> _intensities, double _realHeight, double _realWidth) {
+FresnelIntegral::FresnelIntegral(double _lambda, double _z, const std::vector<std::vector<ComplexNumber>> _intensities, double _realHeight, double _realWidth) {
 	lambda = _lambda;
 	z = _z;
 	k = 2 * std::acos(-1) / lambda;
@@ -10,19 +10,19 @@ FresnelIntegral::FresnelIntegral(double _lambda, double _z, const std::vector<st
 	dx = realWidth / _intensities[0].size();
 }
 
-std::vector<std::vector<double>> FresnelIntegral::integrate() {
-	std::vector<std::vector<double>> integratingResult(intensities.size(), std::vector<double>(intensities[0].size()));
+std::vector<std::vector<ComplexNumber>> FresnelIntegral::integrate() {
+	std::vector<std::vector<ComplexNumber>> integratingResult(amplitudes.size(), std::vector<ComplexNumber>(amplitudes[0].size()));
 
 	concurrency::parallel_for(size_t(0), integratingResult.size(), [&](size_t y) {
 		concurrency::parallel_for(size_t(0), integratingResult[y].size(), [&](size_t x) {
 			ComplexNumber result(0, 0);
-			for (size_t yprime(0); yprime < intensities.size(); ++yprime) {
-				for (size_t xprime(0); xprime < intensities[yprime].size(); ++xprime) {
-					result += intensities[yprime][xprime] * complexExp(k * (std::pow(x - xprime, 2) + std::pow(y - yprime, 2)) / (2 * z)) * dx * dy;
+			for (size_t yprime(0); yprime < amplitudes.size(); ++yprime) {
+				for (size_t xprime(0); xprime < amplitudes[yprime].size(); ++xprime) {
+					result += amplitudes[yprime][xprime] * complexExp(k * (std::pow(x - xprime, 2) + std::pow(y - yprime, 2)) / (2 * z)) * dx * dy;
 				}
 			}
 			result *= I * complexExp(k * z) / (lambda * z);
-			integratingResult[y][x] = result.abs();
+			integratingResult[y][x] = result;
 		});
 	});
 
--- Интеграл Френеля/FresnelIntegral.h
+++ Интеграл Френеля/FresnelIntegral.h
@@ -7,15 +7,15 @@
 
 class FresnelIntegral {
 public:
-	FresnelIntegral(double _lambda, double _z, const std::vector<std::vector<double>> _intensities, double _realHeight, double _realWidth);
+	FresnelIntegral(double _lambda, double _z, const std::vector<std::vector<ComplexNumber>> _intensities, double _realHeight, double _realWidth);
 
-	std::vector<std::vector<double>> integrate();
+	std::vector<std::vector<ComplexNumber>> integrate();
 
 private:
 	double lambda; // длина волны
 	double z; // расстояние до изображения
 	double k; // волновое число
-	std::vector<std::vector<double>> intensities; // исходное распределение интенсивностей
+	std::vector<std::vector<ComplexNumber>> amplitudes; // исходное распределение интенсивностей
 	double realHeight; // физическая высота изображения
 	double realWidth; // физическая ширина изображения
 	double dx;
